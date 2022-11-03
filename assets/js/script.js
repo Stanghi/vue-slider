9b2ecea318c83b8f5e3bd0a012a5e1cc255a3829
Stanghi <stangherlin.matti@gmail.com>
2022-11-03 13:34:28 +0100

    [init]

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+const f1pilot = [
+    {
+        name: 'Lewis',
+        lastname: 'Hamilton',
+        team: 'Mercedes AMG',
+        img: 'hamilton.jpg'
+    },
+    {
+        name: 'Charles',
+        lastname: 'Leclerc',
+        team: 'Ferrari',
+        img: 'leclerc.jpg'
+    },
+    {
+        name: 'Lando',
+        lastname: 'Norris',
+        team: 'McLaren',
+        img: 'norris.jpg'
+    },
+    {
+        name: 'George',
+        lastname: 'Russel',
+        team: 'Mercedes AMG',
+        img: 'russel.jpg'
+    },
+    {
+        name: 'Max',
+        lastname: 'Verstappen',
+        team: 'RedBull',
+        img: 'verstappen.jpg'
+    }
+];
